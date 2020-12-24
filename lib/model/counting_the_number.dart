e82711d5ddd6c7b18951836f6eda2153b5857d65
Sanjib <sanjib12sinha@gmail.com>
2020-12-24 12:33:26 +0530

    next commit

--- lib/model/counting_the_number.dart
+++ lib/model/counting_the_number.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 class CountingTheNumber with ChangeNotifier {
   int number = 0;
   String message = 'Sanjib Sinha';
+
   void increaseNumber(int number) {
     number++;
     notifyListeners();
